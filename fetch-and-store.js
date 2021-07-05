643e32af930c32507529b386eeda086749a685dd
Kenneth Tsang <me@jxeeno.com>
2021-07-05 12:57:24 +1000

    feat: add validation for dose breakdowns

--- fetch-and-store.js
+++ fetch-and-store.js
@@ -79,6 +79,291 @@ const validateData = (data) => {
         'cwthAgedCareBreakdown.cwthAgedCareDoses.secondDose',
         'cwthAgedCareBreakdown.cwthAgedCareFacilities.firstDose',
         'cwthAgedCareBreakdown.cwthAgedCareFacilities.secondDose',
+
+        'doseBreakdown.national[0].firstDoseCount',
+        'doseBreakdown.national[0].firstDosePct',
+        'doseBreakdown.national[0].secondDoseCount',
+        'doseBreakdown.national[0].secondDosePct',
+        'doseBreakdown.national[0].femalePct',
+        'doseBreakdown.national[0].malePct',
+
+        'doseBreakdown.national[1].firstDoseCount',
+        'doseBreakdown.national[1].firstDosePct',
+        'doseBreakdown.national[1].secondDoseCount',
+        'doseBreakdown.national[1].secondDosePct',
+        'doseBreakdown.national[1].femalePct',
+        'doseBreakdown.national[1].malePct',
+
+        'doseBreakdown.national[2].firstDoseCount',
+        'doseBreakdown.national[2].firstDosePct',
+        'doseBreakdown.national[2].secondDoseCount',
+        'doseBreakdown.national[2].secondDosePct',
+        'doseBreakdown.national[2].femalePct',
+        'doseBreakdown.national[2].malePct',
+
+        'doseBreakdown.national[3].firstDoseCount',
+        'doseBreakdown.national[3].firstDosePct',
+        'doseBreakdown.national[3].secondDoseCount',
+        'doseBreakdown.national[3].secondDosePct',
+        'doseBreakdown.national[3].femalePct',
+        'doseBreakdown.national[3].malePct',
+
+        'doseBreakdown.national[4].firstDoseCount',
+        'doseBreakdown.national[4].firstDosePct',
+        'doseBreakdown.national[4].secondDoseCount',
+        'doseBreakdown.national[4].secondDosePct',
+        'doseBreakdown.national[4].femalePct',
+        'doseBreakdown.national[4].malePct',
+
+        'doseBreakdown.national[5].firstDoseCount',
+        'doseBreakdown.national[5].firstDosePct',
+        'doseBreakdown.national[5].secondDoseCount',
+        'doseBreakdown.national[5].secondDosePct',
+        'doseBreakdown.national[5].femalePct',
+        'doseBreakdown.national[5].malePct',
+
+        'doseBreakdown.national[6].firstDoseCount',
+        'doseBreakdown.national[6].firstDosePct',
+        'doseBreakdown.national[6].secondDoseCount',
+        'doseBreakdown.national[6].secondDosePct',
+        'doseBreakdown.national[6].femalePct',
+        'doseBreakdown.national[6].malePct',
+
+        'doseBreakdown.national[7].firstDoseCount',
+        'doseBreakdown.national[7].firstDosePct',
+        'doseBreakdown.national[7].secondDoseCount',
+        'doseBreakdown.national[7].secondDosePct',
+        'doseBreakdown.national[7].femalePct',
+        'doseBreakdown.national[7].malePct',
+
+        'doseBreakdown.national[8].firstDoseCount',
+        'doseBreakdown.national[8].firstDosePct',
+        'doseBreakdown.national[8].secondDoseCount',
+        'doseBreakdown.national[8].secondDosePct',
+        'doseBreakdown.national[8].femalePct',
+        'doseBreakdown.national[8].malePct',
+
+        'doseBreakdown.national[9].firstDoseCount',
+        'doseBreakdown.national[9].firstDosePct',
+        'doseBreakdown.national[9].secondDoseCount',
+        'doseBreakdown.national[9].secondDosePct',
+        'doseBreakdown.national[9].femalePct',
+        'doseBreakdown.national[9].malePct',
+
+        'doseBreakdown.national[10].firstDoseCount',
+        'doseBreakdown.national[10].firstDosePct',
+        'doseBreakdown.national[10].secondDoseCount',
+        'doseBreakdown.national[10].secondDosePct',
+        'doseBreakdown.national[10].femalePct',
+        'doseBreakdown.national[10].malePct',
+
+        'doseBreakdown.national[11].firstDoseCount',
+        'doseBreakdown.national[11].firstDosePct',
+        'doseBreakdown.national[11].secondDoseCount',
+        'doseBreakdown.national[11].secondDosePct',
+        'doseBreakdown.national[11].femalePct',
+        'doseBreakdown.national[11].malePct',
+
+        'doseBreakdown.national[12].firstDoseCount',
+        'doseBreakdown.national[12].firstDosePct',
+        'doseBreakdown.national[12].secondDoseCount',
+        'doseBreakdown.national[12].secondDosePct',
+        'doseBreakdown.national[12].femalePct',
+        'doseBreakdown.national[12].malePct',
+
+        'doseBreakdown.national[13].firstDoseCount',
+        'doseBreakdown.national[13].firstDosePct',
+        'doseBreakdown.national[13].secondDoseCount',
+        'doseBreakdown.national[13].secondDosePct',
+        'doseBreakdown.national[13].femalePct',
+        'doseBreakdown.national[13].malePct',
+
+        'doseBreakdown.national[14].firstDoseCount',
+        'doseBreakdown.national[14].firstDosePct',
+        'doseBreakdown.national[14].secondDoseCount',
+        'doseBreakdown.national[14].secondDosePct',
+        'doseBreakdown.national[14].femalePct',
+        'doseBreakdown.national[14].malePct',
+
+        'doseBreakdown.national[15].firstDoseCount',
+        'doseBreakdown.national[15].firstDosePct',
+        'doseBreakdown.national[15].secondDoseCount',
+        'doseBreakdown.national[15].secondDosePct',
+        'doseBreakdown.national[15].femalePct',
+        'doseBreakdown.national[15].malePct',
+
+        'doseBreakdown.national[16].firstDoseCount',
+        'doseBreakdown.national[16].firstDosePct',
+        'doseBreakdown.national[16].secondDoseCount',
+        'doseBreakdown.national[16].secondDosePct',
+        'doseBreakdown.national[16].femalePct',
+        'doseBreakdown.national[16].malePct',
+
+
+        'doseBreakdown.NSW[0].firstDoseCount',
+        'doseBreakdown.NSW[0].firstDosePct',
+        'doseBreakdown.NSW[0].secondDoseCount',
+        'doseBreakdown.NSW[0].secondDosePct',
+        'doseBreakdown.NSW[0].cohortPopulation',
+
+        'doseBreakdown.VIC[0].firstDoseCount',
+        'doseBreakdown.VIC[0].firstDosePct',
+        'doseBreakdown.VIC[0].secondDoseCount',
+        'doseBreakdown.VIC[0].secondDosePct',
+        'doseBreakdown.VIC[0].cohortPopulation',
+
+        'doseBreakdown.QLD[0].firstDoseCount',
+        'doseBreakdown.QLD[0].firstDosePct',
+        'doseBreakdown.QLD[0].secondDoseCount',
+        'doseBreakdown.QLD[0].secondDosePct',
+        'doseBreakdown.QLD[0].cohortPopulation',
+
+        'doseBreakdown.WA[0].firstDoseCount',
+        'doseBreakdown.WA[0].firstDosePct',
+        'doseBreakdown.WA[0].secondDoseCount',
+        'doseBreakdown.WA[0].secondDosePct',
+        'doseBreakdown.WA[0].cohortPopulation',
+
+        'doseBreakdown.TAS[0].firstDoseCount',
+        'doseBreakdown.TAS[0].firstDosePct',
+        'doseBreakdown.TAS[0].secondDoseCount',
+        'doseBreakdown.TAS[0].secondDosePct',
+        'doseBreakdown.TAS[0].cohortPopulation',
+
+        'doseBreakdown.SA[0].firstDoseCount',
+        'doseBreakdown.SA[0].firstDosePct',
+        'doseBreakdown.SA[0].secondDoseCount',
+        'doseBreakdown.SA[0].secondDosePct',
+        'doseBreakdown.SA[0].cohortPopulation',
+
+        'doseBreakdown.ACT[0].firstDoseCount',
+        'doseBreakdown.ACT[0].firstDosePct',
+        'doseBreakdown.ACT[0].secondDoseCount',
+        'doseBreakdown.ACT[0].secondDosePct',
+        'doseBreakdown.ACT[0].cohortPopulation',
+
+        'doseBreakdown.NT[0].firstDoseCount',
+        'doseBreakdown.NT[0].firstDosePct',
+        'doseBreakdown.NT[0].secondDoseCount',
+        'doseBreakdown.NT[0].secondDosePct',
+        'doseBreakdown.NT[0].cohortPopulation',
+
+
+        'doseBreakdown.NSW[1].firstDoseCount',
+        'doseBreakdown.NSW[1].firstDosePct',
+        'doseBreakdown.NSW[1].secondDoseCount',
+        'doseBreakdown.NSW[1].secondDosePct',
+        'doseBreakdown.NSW[1].cohortPopulation',
+
+        'doseBreakdown.VIC[1].firstDoseCount',
+        'doseBreakdown.VIC[1].firstDosePct',
+        'doseBreakdown.VIC[1].secondDoseCount',
+        'doseBreakdown.VIC[1].secondDosePct',
+        'doseBreakdown.VIC[1].cohortPopulation',
+
+        'doseBreakdown.QLD[1].firstDoseCount',
+        'doseBreakdown.QLD[1].firstDosePct',
+        'doseBreakdown.QLD[1].secondDoseCount',
+        'doseBreakdown.QLD[1].secondDosePct',
+        'doseBreakdown.QLD[1].cohortPopulation',
+
+        'doseBreakdown.WA[1].firstDoseCount',
+        'doseBreakdown.WA[1].firstDosePct',
+        'doseBreakdown.WA[1].secondDoseCount',
+        'doseBreakdown.WA[1].secondDosePct',
+        'doseBreakdown.WA[1].cohortPopulation',
+
+        'doseBreakdown.TAS[1].firstDoseCount',
+        'doseBreakdown.TAS[1].firstDosePct',
+        'doseBreakdown.TAS[1].secondDoseCount',
+        'doseBreakdown.TAS[1].secondDosePct',
+        'doseBreakdown.TAS[1].cohortPopulation',
+
+        'doseBreakdown.SA[1].firstDoseCount',
+        'doseBreakdown.SA[1].firstDosePct',
+        'doseBreakdown.SA[1].secondDoseCount',
+        'doseBreakdown.SA[1].secondDosePct',
+        'doseBreakdown.SA[1].cohortPopulation',
+
+        'doseBreakdown.ACT[1].firstDoseCount',
+        'doseBreakdown.ACT[1].firstDosePct',
+        'doseBreakdown.ACT[1].secondDoseCount',
+        'doseBreakdown.ACT[1].secondDosePct',
+        'doseBreakdown.ACT[1].cohortPopulation',
+
+        'doseBreakdown.NT[1].firstDoseCount',
+        'doseBreakdown.NT[1].firstDosePct',
+        'doseBreakdown.NT[1].secondDoseCount',
+        'doseBreakdown.NT[1].secondDosePct',
+        'doseBreakdown.NT[1].cohortPopulation',
+
+
+        'doseBreakdown.NSW[2].firstDoseCount',
+        'doseBreakdown.NSW[2].firstDosePct',
+        'doseBreakdown.NSW[2].secondDoseCount',
+        'doseBreakdown.NSW[2].secondDosePct',
+        'doseBreakdown.NSW[2].cohortPopulation',
+
+        'doseBreakdown.VIC[2].firstDoseCount',
+        'doseBreakdown.VIC[2].firstDosePct',
+        'doseBreakdown.VIC[2].secondDoseCount',
+        'doseBreakdown.VIC[2].secondDosePct',
+        'doseBreakdown.VIC[2].cohortPopulation',
+
+        'doseBreakdown.QLD[2].firstDoseCount',
+        'doseBreakdown.QLD[2].firstDosePct',
+        'doseBreakdown.QLD[2].secondDoseCount',
+        'doseBreakdown.QLD[2].secondDosePct',
+        'doseBreakdown.QLD[2].cohortPopulation',
+
+        'doseBreakdown.WA[2].firstDoseCount',
+        'doseBreakdown.WA[2].firstDosePct',
+        'doseBreakdown.WA[2].secondDoseCount',
+        'doseBreakdown.WA[2].secondDosePct',
+        'doseBreakdown.WA[2].cohortPopulation',
+
+        'doseBreakdown.TAS[2].firstDoseCount',
+        'doseBreakdown.TAS[2].firstDosePct',
+        'doseBreakdown.TAS[2].secondDoseCount',
+        'doseBreakdown.TAS[2].secondDosePct',
+        'doseBreakdown.TAS[2].cohortPopulation',
+
+        'doseBreakdown.SA[2].firstDoseCount',
+        'doseBreakdown.SA[2].firstDosePct',
+        'doseBreakdown.SA[2].secondDoseCount',
+        'doseBreakdown.SA[2].secondDosePct',
+        'doseBreakdown.SA[2].cohortPopulation',
+
+        'doseBreakdown.ACT[2].firstDoseCount',
+        'doseBreakdown.ACT[2].firstDosePct',
+        'doseBreakdown.ACT[2].secondDoseCount',
+        'doseBreakdown.ACT[2].secondDosePct',
+        'doseBreakdown.ACT[2].cohortPopulation',
+
+        'doseBreakdown.NT[2].firstDoseCount',
+        'doseBreakdown.NT[2].firstDosePct',
+        'doseBreakdown.NT[2].secondDoseCount',
+        'doseBreakdown.NT[2].secondDosePct',
+        'doseBreakdown.NT[2].cohortPopulation',
+
+
+        'doseBreakdown.AUS[0].firstDoseCount',
+        'doseBreakdown.AUS[0].firstDosePct',
+        'doseBreakdown.AUS[0].secondDoseCount',
+        'doseBreakdown.AUS[0].secondDosePct',
+        'doseBreakdown.AUS[0].cohortPopulation',
+
+        'doseBreakdown.AUS[1].firstDoseCount',
+        'doseBreakdown.AUS[1].firstDosePct',
+        'doseBreakdown.AUS[1].secondDoseCount',
+        'doseBreakdown.AUS[1].secondDosePct',
+        'doseBreakdown.AUS[1].cohortPopulation',
+
+        'doseBreakdown.AUS[2].firstDoseCount',
+        'doseBreakdown.AUS[2].firstDosePct',
+        'doseBreakdown.AUS[2].secondDoseCount',
+        'doseBreakdown.AUS[2].secondDosePct',
+        'doseBreakdown.AUS[2].cohortPopulation',
     ]
 
     let errors = [];
@@ -100,6 +385,18 @@ const validateData = (data) => {
     logErrorNotEq(cwthAgedCareTotal, _.get(data, 'totals.cwthAgedCare.total'), true, 'cwthAgedCareTotal');
     logErrorNotEq(cwthAgedCareTotal + cwthPrimaryCareTotal, _.get(data, 'totals.cwthAll.total'), true, 'cwthPrimaryCareTotal + cwthAgedCareTotal');
     logErrorNotEq(statesTotal + cwthAgedCareTotal + cwthPrimaryCareTotal, _.get(data, 'totals.national.total'), true, 'statesTotal + cwthAgedCareTotal + cwthPrimaryCareTotal');
+
+    // AIR data
+
+    // 16 plus state breakdown vs 16 plus age breakdown
+    const firstDoseCountFromAge = _.get(data, `doseBreakdown.national`, []).reduce((runningTotal, figs) => _.get(figs, `firstDoseCount`, 0) + runningTotal, 0);
+    const secondDoseCountFromAge = _.get(data, `doseBreakdown.national`, []).reduce((runningTotal, figs) => _.get(figs, `secondDoseCount`, 0) + runningTotal, 0);
+
+    const firstDoseCountFromAus16Plus = _.get(data, `doseBreakdown.AUS[0].firstDoseCount`, 0);
+    const secondDoseCountFromAus16Plus = _.get(data, `doseBreakdown.AUS[0].secondDoseCount`, 0);
+
+    logErrorNotEq(firstDoseCountFromAge, firstDoseCountFromAus16Plus, true, 'firstDoseCountFromAus16Plus');
+    logErrorNotEq(secondDoseCountFromAge, secondDoseCountFromAus16Plus, true, 'secondDoseCountFromAus16Plus');
 
     if(errors.length){
         console.error(errors)
