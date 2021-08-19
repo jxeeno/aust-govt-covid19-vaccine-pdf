745362d2d1d87fe2eb23db0384f28dc578abae63
Kenneth Tsang <me@jxeeno.com>
2021-08-19 19:02:32 +1000

    fix: multi PDF using index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const AusDeptHealthVaccinePdf = require('./vaccinepdf');
 const axios = require('axios');
 
-const scrapeyscrape = async url => {
+const scrapeyscrape = async (url, url2) => {
   console.log('attempting to fetch ' + url);
   const { data } = await axios.get(url, {
     responseType: 'arraybuffer'
   });
 
-  console.log('got pdf from website');
+  let data2;
+  if(url2){
+    console.log('attempting to fetch ' + url2);
+    const { data } = await axios.get(url2, {
+      responseType: 'arraybuffer'
+    });
+    if(data){
+      data2 = data;
+    }
+  }
+
+  console.log('got pdfs from website');
   const vpdf = new AusDeptHealthVaccinePdf();
-  await vpdf.parsePdf(data);
-  console.log('parsed pdf');
+  const resp = await vpdf.parsePdf(data, data2);
+  console.log('parsed pdfs');
+  console.log(JSON.stringify(resp, null, 2))
 };
 
-scrapeyscrape(process.argv[2]);
+scrapeyscrape(process.argv[2], process.argv[3]);
